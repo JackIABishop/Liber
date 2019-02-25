72245069bab1b452c3cc559eaf9e6c41f2b86636
Jack Bishop <jack.bish96@gmail.com>
2019-02-25 20:19:02 +0000

    Functionality to allow the user to delete their bookcase data.

--- Liber/Support Files/Utilities/GradientLayer.swift
+++ Liber/Support Files/Utilities/GradientLayer.swift
@@ -11,8 +11,17 @@ import UIKit
 
 var gradientLayer: CAGradientLayer!
 
+// Blue
 let colorTop = UIColor(red: 192.0 / 255.0, green: 220.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0).cgColor
 let colorBottom = UIColor(red: 3.0 / 255.0, green: 150.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0).cgColor
+
+// Grey
+//let colorTop = UIColor(red: 144.0 / 255.0, green: 247.0 / 255.0, blue: 255.0 / 236.0, alpha: 1.0).cgColor
+//let colorBottom = UIColor(red: 50.0 / 255.0, green: 204.0 / 255.0, blue: 188.0 / 255.0, alpha: 1.0).cgColor
+
+//
+//let colorTop = UIColor(red: 249.0 / 255.0, green: 119.0 / 255.0, blue: 148.0 / 236.0, alpha: 1.0).cgColor
+//let colorBottom = UIColor(red: 98.0 / 255.0, green: 58.0 / 255.0, blue: 162.0 / 255.0, alpha: 1.0).cgColor
 
 func createGradientLayer(view: UIView) {
     gradientLayer = CAGradientLayer()
